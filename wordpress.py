3d7438906c99d3e0071750733e13db5a16228218
user-ansible <cottet.christophe@gmail.com>
2021-07-14 09:41:46 +0200

    test

--- wordpress.py
+++ wordpress.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-from ansible import context
-from ansible.module_utils._text import to_text
-from ansible.cli.playbook import PlaybookCLI
+import ansible
 
-# Read all the arguments from the cli
-args = [to_text(a, errors='surrogate_or_strict') for a in sys.argv]
 
-# Pass them to the playbook cli object
-pb_cli = PlaybookCLI(args)
-
-# Parse the arguments
-pb_cli.parse()
-
-# The context should now contain all of our parsed arguments
-print(context.CLIARGS)
