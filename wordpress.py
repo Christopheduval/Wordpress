d11badaf2e37bbb5d6ccd2b556d8cf56c770a06b
user-ansible <cottet.christophe@gmail.com>
2021-07-15 14:50:46 +0200

    version 1 - execution playbook

--- wordpress.py
+++ wordpress.py
@@ -2,5 +2,11 @@
 
 import os
 
+#mise a jour du fichier inventaire.ini
+
+#préparation des nodes
+
+#execution playbook
+
 os.system("ansible-playbook -i inventaire.ini --user user-ansible --become --ask-become-pass install-wordpress.yml")
 
