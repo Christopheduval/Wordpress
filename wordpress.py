20092af8e42768b9a0a76d182a1c4d7aba12b83f
user-ansible <cottet.christophe@gmail.com>
2021-07-16 16:33:54 +0200

    version 1 - ajout node dans fichier hosts

--- wordpress.py
+++ wordpress.py
@@ -5,14 +5,24 @@ import os
 
 #mise a jour du fichier inventaire.ini
 
+print("Voici le contenu actuel du fichier inventaire:")
+
+with open('inventaire.ini') as f:
+    for line in f:
+        print(line)
+
 rep=input("Voulez-vous ajouter un noeud à l'inventaire ? (O/N) ")
 
-if rep != "N":
+while rep != "N":
     host=input("Entrer le nom du noeud: ")
-    with open('inventaire.ini', 'a') as f:
-        s = str(host)
-        f.write(s)
-
+    ip=input("Entrer son adresse ip: ")
+    with open('inventaire.ini', 'a') as f1:
+        s1 = str(host)
+        f1.write(s1+'\n')
+    with open('/etc/hosts', 'a') as f2:
+        s2 = str (ip)
+        f2.write(s2+" "+s1+"\n")
+    rep=input("Voulez-vous ajouter un autre noeud à l'inventaire ? (O/N) ")
 
 
 #préparation des nodes
