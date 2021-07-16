1f065e94cb927cb0767cc2969173fff1ff2b8d0f
user-ansible <cottet.christophe@gmail.com>
2021-07-16 11:17:27 +0200

    version 1 - copie clé ssh

--- wordpress.py
+++ wordpress.py
@@ -5,8 +5,15 @@ import os
 #mise a jour du fichier inventaire.ini
 
 #préparation des nodes
+os.system("ssh-copy-id -i ~/.ssh/id_ecdsa.pub administrateur@srvubuntu")
+os.system("ssh-copy-id -i ~/.ssh/id_ecdsa.pub administrateur@http")
+
+
+#os.system("ansible-playbook -i inventaire.ini -m user -a 'name=user-ansible' --user administrateur --ask-pass --become --ask-become-pass all")
+
+#personnalisation de wordpress
 
 #execution playbook
 
-os.system("ansible-playbook -i inventaire.ini --user user-ansible --become --ask-become-pass install-wordpress.yml")
+os.system("ansible-playbook -i inventaire.ini --user administrateur --become --ask-become-pass install-wordpress.yml")
 
