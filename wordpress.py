7cabea5065588ae42e38266f7f05dc119d475861
Christopheduval <84678344+Christopheduval@users.noreply.github.com>
2021-07-20 14:07:37 +0200

    Update wordpress.py

--- wordpress.py
+++ wordpress.py
@@ -53,35 +53,7 @@ os.system("cp wordpress/files/modele-wp-config.php wordpress/files/wp-config.php
 
 wordpress_db = "wordpress"
 wordpress_user = "wordpress"
-wordpress_password = "wordpress"
-
-EffaceEcran()
-reponse=input("Souhaitez-vous personnaliser l'installation de wordpress ? (O/N) (Par défaut, la base de donnée créée sera wordpress avec un utilisateur et un mot de passe identique) : ")
-if reponse == "O":
-    wordpress_db=input("saisir le nom de la base de données: ")
-    wordpress_user=input("saisir le nom de l'utilisateur: ")
-    wordpress_password=input("saisir le mot de passe: ")
-
-with open('wordpress/defaults/main.yml', 'w') as f:
-    f.write('---\n')
-    f.write('wordpress_db: "'+wordpress_db+'"\n')
-    f.write('wordpress_user: "'+wordpress_user+'"\n')
-    f.write('wordpress_password: "'+wordpress_password+'"')
-
-#mise à jour du fichier wp-config.php
-os.system("cp wordpress/files/wp-config-debut.php wordpress/files/wp-config.php")
-with open('wordpress/files/wp-config.php', 'a') as f:
-    f.write("define( 'DB_NAME', '"+wordpress_db+"' );\n")
-    f.write("define( 'DB_USER', '"+wordpress_user+"' );\n")
-    f.write("define( 'DB_PASSWORD', '"+wordpress_password+"' );\n")
-os.system("cat wordpress/files/wp-config-fin.php >> wordpress/files/wp-config.php")
-
-
-
-#execution playbook
-os.system("ansible-playbook -i inventaire.ini --user administrateur --become --ask-become-pass install-wordpress.yml")
-#!/usr/bin/env python3
-
+wordpress_password
 import os
 
 #fonction pour effacer l'écran
