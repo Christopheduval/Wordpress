7923e28952754e70e97f4d2147d3af8126d3ae62
Christopheduval <84678344+Christopheduval@users.noreply.github.com>
2021-07-20 14:11:50 +0200

    Update wordpress.py

--- wordpress.py
+++ wordpress.py
@@ -6,60 +6,6 @@ import os
 def EffaceEcran():
     os.system("clear")
 
-#mise a jour du fichier inventaire.ini
-EffaceEcran()
-print("Voici le contenu actuel du fichier inventaire:\n")
-
-with open('inventaire.ini') as f:
-    for line in f:
-        print(line)
-
-
-rep=input("Voulez-vous ajouter un noeud à l'inventaire ? (O/N) ")
-EffaceEcran()
-
-while rep != "N":
-    host=input("Entrer le nom du noeud: ")
-    ip=input("Entrer son adresse ip: ")
-    with open('inventaire.ini', 'a') as f1:
-        s1 = str(host)
-        f1.write(s1+'\n')
-    with open('/etc/hosts', 'a') as f2:
-        s2 = str (ip)
-        f2.write(s2+" "+s1+"\n")
-    rep=input("Voulez-vous ajouter un autre noeud à l'inventaire ? (O/N) ")
-
-
-#préparation des nodes
-
-#verification de la connexion
-print("test de ping")
-print("")
-with open('inventaire.ini') as f:
-    for line in f:
-        os.system("ping -c 3 -q "+line) 
-
-#copie de clé ssh sur les nodes
-#boucle sur l'ensemble des nodes du fichier inventaire
-EffaceEcran()
-print("copie de la clé publique ssh sur les nodes")
-
-with open('inventaire.ini') as f:
-    for line in f:
-        os.system("ssh-copy-id -i ~/.ssh/id_ecdsa.pub administrateur@"+line)
-
-#personnalisation de wordpress
-os.system("cp wordpress/files/modele-wp-config.php wordpress/files/wp-config.php")
-
-wordpress_db = "wordpress"
-wordpress_user = "wordpress"
-wordpress_password
-import os
-
-#fonction pour effacer l'écran
-def EffaceEcran():
-    os.system("clear")
-
 def Menu():
     print("1. Afficher le contenu du fichier inventaire")
     print("2. Effacer le contenu du fichier inventaire")
@@ -108,9 +54,6 @@ def EffaceInventaire():
     print("Tapez sur la touche Entrée pour retourner au menu principal")
     input()
     Menu()
-
-#Ajouter des noeuds au fichier inventaire.ini
-def AjoutInventaire():
     EffaceEcran()
     rep = "O"
     while rep != "N":
